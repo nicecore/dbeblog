f9e7498d942b3e365914f28cbd69e4f88a518f98
Nicecore <adamdcameron@gmail.com>
2018-05-29 22:45:10 +0000

    test from c9

--- blog/models.py
+++ blog/models.py
@@ -35,3 +35,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+        
+        
+    def get_absolute_url(self):
+        # Return the absolute URL of each individual post object
+        # Returns a reverse() to the post_detail view and provides keyword arguments
+        # with the times coming from strftime
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.strftime('%m'),
+                self.publish.strftime('%d'),
+                self.slug
+            ]
+        )
