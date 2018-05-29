11512134c0f2770abb4f4a007bb8357d7ab7e891
Adam Cameron <adamdcameron@gmail.com>
2018-05-29 15:30:45 -0500

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Post
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    # Creates lookup widget for author, rather than dropdown menu
+    raw_id_fields = ('author',)
+
+
+
+admin.site.register(Post, PostAdmin)
+
+
+
+
